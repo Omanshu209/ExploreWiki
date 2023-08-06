0d25927394db5565462b81c8cbd92cc2abf0a69a
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-08-06 13:41:10 +0530

    Improved the results using `langchain`

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
 import webbrowser as wb
-import wikipedia as info
+from langchain.tools import WikipediaQueryRun
+from langchain.utilities import WikipediaAPIWrapper
 
 class URLPopup(Popup):
     pass
@@ -10,16 +11,18 @@ class URLPopup(Popup):
 class MainApp(MDApp):
 	
 	def build(self):
+		self.wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+		self.question = ""
 		self.theme_cls.theme_style = 'Dark' 
 		self.theme_cls.primary_palette = 'DeepPurple'
 		return Builder.load_file("Design.kv")
 		
 	def search(self):
-		question = self.root.ids.search_bar.text
+		self.question = self.root.ids.search_bar.text
 		try:
-			self.root.ids.output_screen.text = info.summary(question)
+			self.root.ids.output_screen.text = self.wikipedia.run(self.question)
 		except Exception:
-			self.root.ids.output_screen.text = self.error_msg(question)
+			self.root.ids.output_screen.text = self.error_msg(self.question)
 	
 	def error_msg(self,topic):
 		return f"ERROR\n\n\nPossible reasons for the error:\n\n1) No Internet Connection\n2) Spelling of \"{topic}\" might be incorrect \n3) Information might not be available for \"{topic}\"\n4) The server might be under maintainance"
